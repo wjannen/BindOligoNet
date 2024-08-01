f4e5d9ff2726622ca1c380edf64a80f9516aa528
bill <jannen@cs.williams.edu>
2024-08-01 10:57:36 -0400

    adding default zshrc environment variable setup in binset

--- bin/binset.sh
+++ bin/binset.sh
@@ -3,3 +3,7 @@
 export BINPATH="`pwd | sed '/bin\/\{0,1\}$/!s/\/\{0,1\}$/\/bin\//'`"
 sed -i '' '/BINPATH/d' ~/.bash_profile
 echo "export BINPATH=$BINPATH" >> ~/.bash_profile
+
+touch ~/.zshrc
+sed -i '' '/BINPATH/d' ~/.zshrc
+echo "export BINPATH=$BINPATH" >> ~/.zshrc
